f5788ee060577c476893c38c62b2343cf9b7782f
sanjogbhalla16 <sanjog.bhalla@gmail.com>
2024-05-11 12:33:02 +0530

    MIddleware All done

--- backend/middleware.js
+++ backend/middleware.js
@@ -13,9 +13,9 @@ const jwt = require("jsonwebtoken");
 const authMiddleware = (req, res, next) => {
   const authHeader = req.headers.authorization;
 
-  console.log(req.headers);
+  // console.log(req.headers);
   //if this authHeader does not work
-  console.log(authHeader);
+  // console.log(authHeader);
 
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res.status(403).json({
