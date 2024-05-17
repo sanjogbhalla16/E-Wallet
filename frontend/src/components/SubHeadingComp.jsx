3fc8a2e0ace6bc8391aac96255882d9e4b740982
sanjogbhalla16 <sanjog.bhalla@gmail.com>
2024-05-17 23:46:45 +0530

    changes in frontend for backed

--- frontend/src/components/SubHeadingComp.jsx
+++ frontend/src/components/SubHeadingComp.jsx
@@ -1,3 +1,5 @@
-export function SubHeading() {
-  return <div className="text-slate-500 text-md pt-1 px-4 pb-4"></div>;
+export function SubHeading({ subHeading }) {
+  return (
+    <div className="text-slate-500 text-md pt-1 px-4 pb-4">{subHeading}</div>
+  );
 }
