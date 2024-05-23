89c28e7214ee2c791d73c9ca8701c42b9bd02bde
sanjogbhalla16 <sanjog.bhalla@gmail.com>
2024-05-23 21:57:35 +0530

    changes in the front end for the backend

--- frontend/src/components/UserComp.jsx
+++ frontend/src/components/UserComp.jsx
@@ -1,21 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "./Button";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export const Users = () => {
   // Replace with backend call
-  const [users, setUsers] = useState([
-    {
-      firstName: "Harkirat",
-      lastName: "Singh",
-      _id: 1,
-    },
-  ]);
+  const [users, setUsers] = useState([]);
+  const [filter, setFilter] = useState(""); //these are for the username to be displayed
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/api/v1/user/bulk?filter=" + filter)
+      .then((response) => {
+        setUsers(response.data.user);
+      });
+  }, [filter]);
 
   return (
     <>
       <div className="font-bold mt-6 text-lg">Users</div>
       <div className="my-2">
         <input
+          onChange={(e) => {
+            setFilter(e.target.value);
+          }}
           type="text"
           placeholder="Search users..."
           className="w-full px-2 py-1 border rounded border-slate-200"
@@ -30,7 +38,9 @@ export const Users = () => {
   );
 };
 
-function User({ user }) {
+export function User({ user }) {
+  const navigate = useNavigate();
+
   return (
     <div className="flex justify-between">
       <div className="flex">
@@ -47,7 +57,13 @@ function User({ user }) {
       </div>
 
       <div className="flex flex-col justify-center h-ful">
-        <Button label={"Send Money"} />
+        <Button
+          onClick={(e) => {
+            navigate("/send?id=" + user._id + "&name=" + user.firstName); //this will help us navigate
+            //us to send money page
+          }}
+          label={"Send Money"}
+        />
       </div>
     </div>
   );
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,8 +2,17 @@ import React, { useEffect, useState } from "react";
 import { AppBar } from "../components/AppBarComp";
 import { Balance } from "../components/BalanceComp";
 import { Users, User } from "../components/UserComp";
+import axios from "axios";
 
 function Dashboard() {
+  const [users, setUsers] = useState([{}]);
+
+  //now we need to fetch these values whenever someone will type users in the app bar
+  useEffect(() => {
+    axios.get("http://localhost:3000/api/v1/user/").then((response) => {
+      setUsers(response.data.user);
+    });
+  }, [users]);
   return (
     <div>
       <AppBar></AppBar>
