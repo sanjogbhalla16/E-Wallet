586066a90134ae24fc76db2f4eb870f2f53099fd
sanjogbhalla16 <sanjog.bhalla@gmail.com>
2024-05-23 23:32:31 +0530

    changes in the frontend for the backend

---
+++ frontend/src/pages/Landing.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { GiWallet } from "react-icons/gi";
+import { useNavigate } from "react-router-dom";
+
+function Landing() {
+  const navigate = useNavigate();
+  return (
+    <div className="bg-zinc-800 h-screen w-full flex flex-col justify-center items-center">
+      <div className="text-8xl font-bold flex items-center text-white my-12">
+        E-Wallet <GiWallet />
+      </div>
+      <button
+        onClick={() => navigate("/signup")}
+        className="text-3xl text-black rounded-xl p-2 hover:bg-black duration-300 hover:text-white  bg-white"
+      >
+        Get Started
+      </button>
+    </div>
+  );
+}
+
+export default Landing;
